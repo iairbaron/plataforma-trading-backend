52132eda53e4730c49d83c84b4eaff5b658df309
iairbaron <mauro@fusioncloud.ai>
2025-04-19 19:53:48 -0300

    vercel conf

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
-app.use("/api/auth", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 app.use("/api/market", marketRoutes);
 
 app.get("/", (req, res) => {
