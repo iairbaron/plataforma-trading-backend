58a456bc9d4bdda28d0494609c16faf1a5a8c378
iairbaron <mauro@fusioncloud.ai>
2025-04-19 14:10:43 -0300

    instruments controller

--- src/routes/market.routes.ts
+++ src/routes/market.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware";
+import { getInstruments } from "../controllers/market.controller";
 
 const router = Router();
 
 // Obtener todos los instrumentos con sus datos actuales
-router.get("/instruments", authMiddleware);
+router.get("/instruments", authMiddleware, getInstruments);
 
 export default router;
